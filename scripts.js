13b9f0ad33f65a12da2ab1b902e878abdedbe703
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-04-06 18:20:45 -0400

    fix: Dimension var now sourced from CSS

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
 function createGrid()
 {
-    const dimension = 16;
+    const dimension = parseInt(getComputedStyle(document.documentElement)
+    .getPropertyValue('--rowWidth').trim(), 10);
+
     const gridContainer = document.getElementById('js-grid');
     for (let width = 0; width < dimension; ++width)
     {
         for (let height = 0; height < dimension; ++height)
         {
             const gridSquare = document.createElement('div');
-            gridSquare.textContent = `Div ${height}`;
+            gridSquare.textContent = `Div`;
             gridContainer.append(gridSquare);
         }
     }
